9ff5ff693a1432ca04da7b5c6c1a761243227a2b
Bektenovich <150505695+Bektenovich@users.noreply.github.com>
2024-12-12 02:58:48 +0600

    Delete MovieTicket-Booking directory

--- MovieTicket-Booking/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.movieticketbooking {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.movieticketbooking to javafx.fxml;
-    exports org.example.movieticketbooking;
-}
